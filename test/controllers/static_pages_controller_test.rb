5e6f26e970e738f79c20eea153acf882dcf7d786
DanaSelene <dawanamo@gmail.com>
2020-06-12 00:30:24 +0000

    Finish Static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @base_title = "Clock+"
+  end
+  
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Clock+"
   end
 
   test "should get contact_us" do
     get static_pages_contact_us_url
     assert_response :success
+    assert_select "title", "Contact Us | Clock+"
   end
 
 end
